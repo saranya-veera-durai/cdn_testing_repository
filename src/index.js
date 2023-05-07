8b245f19706d87e3fa193a798ad739b529aeac29
saranyaveeradurai <saranya@dcsipl.com>
2023-05-07 14:27:54 +0530

    cdn testing command added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 console.log("HIII HELLLO THIS IS LINE IS CAME FROM MY FIRST OWN CDN :)")
+
+
+// you can test using this code in ur local
+// ------------------------------------------
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+// </head>
+// <body>
+// <script src="https://cdn.jsdelivr.net/gh/saranya-veera-durai/cdn_testing_repository/src/index.js"></script>
+// </body>
+// </html>
